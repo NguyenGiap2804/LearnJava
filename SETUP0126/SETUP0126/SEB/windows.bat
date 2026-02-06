ff91003f738d59e4711e216e5f2bde3831d59a05
NguyenGiap2804 <nguyenxuannguyengiap@gmail.com>
2026-02-06 09:41:35 +0700

    Delete SETUP0126/SETUP0126/SEB directory

--- SETUP0126/SETUP0126/SEB/windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set PAC_URL=https://tech.stromez.tech/proxy.pac
-set CERTFILE=%~dp0cert\mitmproxy-ca-cert.cer
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v AutoConfigURL /t REG_SZ /d %PAC_URL% /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
-certutil -user -addstore "Root" "%CERTFILE%"
-exit
